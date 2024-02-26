8b3bbb4e06446cbac5a8fe2edc1a3ca4daa628cf
Alyona Lapinskaya <alena.ilinskaya1201@gmail.com>
2024-02-26 18:21:14 +0200

    homework 2-product

--- src/route/index.js
+++ src/route/index.js
@@ -1,5 +1,6 @@
 // Підключаємо технологію express для back-end сервера
-const express = require('express')
+const express = require('express');
+const { NormalModule } = require('webpack');
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
 
@@ -31,13 +32,13 @@ class User {
       const index = this.#list.findIndex(
         (user) => user.id === id
         )
-        if(index !== -1) {
-          this.#list.splice(index, 1)
-          return true
-        }
-        else {
-          return false
-        }
+      if(index !== -1) {
+        this.#list.splice(index, 1)
+        return true
+      }
+      else {
+        return false
+      }
     }
 
     static updateById = (id, data) => {
@@ -57,6 +58,43 @@ class User {
         user.email = email 
       }
     }
+}
+
+//=================================================================
+
+class Product {
+  static #list = [];
+
+  constructor(name, price, description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.createData = new Date().toISOString();
+    this.id = Math.floor(10000 + Math.random() * 90000);
+  }
+
+  static add = (product) => {
+    this.#list.push(product);
+  }
+
+  static getList = () =>  this.#list;
+
+  static getById = (id) => 
+    this.#list.find((product) => product.id === id)
+
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (product) => product.id === id
+      )
+    if(index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
 }
 
 //=================================================================
@@ -81,6 +119,25 @@ router.get('/', function (req, res) {
   })
   // ↑↑ сюди вводимо JSON дані
 })
+
+//=========================================================================
+
+router.get('/product-list', function (req, res) {
+
+  const list = Product.getList();
+  
+  res.render('product-list', {
+    style: 'product-list',
+    info: 'Список товарів',
+    list: list,
+  })
+})
+
+router.get('/product-create', function (req, res) {
+   res.render('product-create', {
+    style: 'product-create'
+   });
+});
 
 // ================================================================
 
@@ -131,6 +188,61 @@ router.post('/user-update', function (req, res) {
   })
 })
 
+//=========================================================================
 
+router.post('/product-create', function (req, res) {
+  const {name, price, description} = req.body;
+  
+  const product = new Product(name, price, description);
+  Product.add(product);
+  
+  res.render('alert', {
+    style: 'alert',
+    info: 'Успішне виконання дії',
+    details: 'Товар успішно створений',
+  })
+})
+
+//=========================================================================
+
+router.get('/product-edit', function (req, res) {
+  const { id } = req.query;
+  
+  const product = Product.getById(Number(id));
+
+  res.render('product-edit', {
+    style: 'product-edit',
+    product: product,
+  })
+})
+
+router.get('/product-delete', function (req, res) {
+  const { id } = req.query;
+
+  Product.deleteById(Number(id))
+
+  res.render('alert', {
+    style: 'alert',
+    info: 'Успішне виконання дії',
+    details: 'Товар успішно видалений',
+  })
+})
+//=========================================================================
+
+router.post('/product-edit', function (req, res) {
+  const {id, name, price, description} = req.body;
+  
+  const product = Product.getById(Number(id));
+  product.name = name;
+  product.price = price;
+  product.description = description;
+  
+  res.render('alert', {
+    style: 'alert',
+    info: 'Успішне виконання дії',
+    details: 'Товар успішно оновлений',
+  })
+})
+//=========================================================================
 // Підключаємо роутер до бек-енду
 module.exports = router
